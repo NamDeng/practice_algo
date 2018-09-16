d9acfc45d61b6b33080f383ce8f28c5e3f4d25fd
Nam Won Hyung <nwh0526@gmail.com>
2018-09-16 12:08:13 +0900

    update magic number 제거 및 소스 중복 제거

--- programmers/level1/시저암호.js
+++ programmers/level1/시저암호.js
@@ -1,11 +1,24 @@
+const ASCII = {
+    UPPER: {
+        MIN: 65,
+        MAX: 90
+    },
+    LOWER: {
+        MIN: 97,
+        MAX: 122
+    }
+}
+
 function solution(str, length) {
     return str.replace(/(\w)/g, (s) => {
-        const ascii = s.charCodeAt(0);
-        if(ascii <= 90 && length > (90 - ascii)) { // 대문자 65~90
-            return String.fromCharCode(64 + (length - (90 - ascii)));
-        } else if(ascii <= 122 && length > (122 - ascii)) { // 소문자 97~122
-            return String.fromCharCode(96 + (length - (122 - ascii)));
-        } else return String.fromCharCode(ascii + length);
+        let ascii = s.charCodeAt(0);
+        if(ascii <= ASCII.UPPER.MAX && length > (ASCII.UPPER.MAX - ascii))  // 대문자 65~90
+            ascii = ASCII.UPPER.MIN-1 + (length - (ASCII.UPPER.MAX - ascii));
+        else if(ascii <= ASCII.LOWER.MAX && length > (ASCII.LOWER.MAX - ascii))  // 소문자 97~122
+            ascii = ASCII.LOWER.MIN-1 + (length - (ASCII.LOWER.MAX - ascii));
+        else ascii += length;
+        
+        return String.fromCharCode(ascii);
     });
 }
 console.log(solution("a B z Z", 1));
