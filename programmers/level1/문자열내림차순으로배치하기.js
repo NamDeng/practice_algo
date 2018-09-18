43af977a12727022e747399828a09c55e822e3ed
Nam Won Hyung <nwh0526@gmail.com>
2018-09-18 21:17:35 +0900

    문자열내림차순으로배치하기 채점결과 한개 런타임 에러...

---
+++ programmers/level1/문자열내림차순으로배치하기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    const upper = s.match(/[A-Z]/g);
+    const lower = s.match(/[a-z]/g);
+
+    return sort(lower) + sort(upper);
+}
+function sort(array) {
+    return array.sort((pre, next) => {
+        if(pre < next) return 1;
+        else if(pre > next) return -1;
+        return 0;
+    }).join('');
+}
+console.log(solution("Zbcdefg"));
